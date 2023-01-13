408a26d7cbd78fb29973ba1dc63040676b46cb6d
Andrea <andrearivas4cinfor@gmail.com>
2023-01-13 20:47:48 +0100

    mobile version

--- main.js
+++ main.js
@@ -13,8 +13,12 @@ const addSection = document.querySelector('.add-section');
 const contactSection = document.querySelector('.contact-section');
 const dateToday = document.querySelector('.date-display');
 const checkbox = document.querySelector('input[name="checkbox"]');
-
-
+const successMessage = document.querySelector('.success-message');
+const errorMessage = document.querySelector('.error-message')
+const openModal = document.querySelector('.add-book-mobile');
+const modal = document.querySelector('.form-container');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
 let newId = 0;
 
 // class constructor of the book object
@@ -63,10 +67,10 @@ class UI {
       <p id="info-book" class="book-title">${newBook.title}</p>
       <p id="info-book" class="book-author">${newBook.author}</p>
       <p id="pages" class="book-pages">${newBook.pages}</p>
-      <div id="pages">
+      <div id="readornot">
       <p class="read">READ</p>
       </div>
-      <button id="${newBook.id}" class="remove-btn"><i class="fa-regular fa-trash-can fa-xl"></i></button>
+      <button id="${newBook.id}" class="remove-btn">x</button>
       <div class="line-bottom"></div>
     </div>
   `;
@@ -79,9 +83,11 @@ class UI {
       <p id="info-book" class="book-author">${newBook.author}</p>
       <p id="pages" class="book-pages">${newBook.pages}</p>
       <div>
-      <p id="pages" class="not-read">Not Yet</p>
+      <p id="readornot" class="not-read">Not Yet</p>
       </div>
-      <button id="${newBook.id}" class="remove-btn"><i class="fa-regular fa-trash-can fa-xl"></i></button>
+      <button id="${newBook.id}" class="remove-btn">
+     x
+      </button>
       <div class="line-bottom"></div>
     </div>
   `;
@@ -95,6 +101,15 @@ class UI {
     newTitleInput.value = '';
     newAuthorInput.value = '';
     newPagesInput.value = '';
+  }
+
+  static successMsg() {
+    successMessage.style.display = 'flex';
+    setTimeout(() => successMessage.remove(), 3000);
+  }
+  static errorMsg() {
+    errorMessage.style.display = 'flex';
+    setTimeout(() => errorMessage.remove(), 3000);
   }
 }
 const addBookPressed = function (e) {
@@ -113,6 +128,7 @@ const addBookPressed = function (e) {
   if (newTitle && newAuthor && newPages ) {
     if (checkbox.checked){
       const newBook = new Book(newTitle, newAuthor, newPages, true,newId); //
+      UI.successMsg();
       Storage.addBook(newBook);
     UI.addBooktoLibrary(newBook);
     UI.clearFields();
@@ -120,13 +136,32 @@ const addBookPressed = function (e) {
     } else {
       const newBook = new Book(newTitle, newAuthor, newPages, false, newId); //
       Storage.addBook(newBook);
+      UI.successMsg();
     UI.addBooktoLibrary(newBook);
     UI.clearFields();
     
-    }
+    } 
   
+  } else {
+    UI.errorMsg();
   }
 };
+
+const openModalF = function() {
+  modal.style.display = 'flex';
+  modal.style.position = "fixed";
+  modal.style.width = "100%";
+  modal.style.height = "100%";
+  modal.style.backgroundColor = "white"
+  modal.style.padding = "0"
+  openModal.style.display = "none"
+  
+}
+
+const closeModalF = function() {
+  modal.style.display = "none";
+  openModal.style.display = "block"
+}
 // Function to remove new Book
 const removeBook = function (e) {
   if (e.target.classList.contains('remove-btn')) {
@@ -141,3 +176,5 @@ const removeBook = function (e) {
 addBtn.addEventListener('click', addBookPressed);
 libraryContainer.addEventListener('click', removeBook);
 document.addEventListener('DOMContentLoaded', UI.displayBook);
+openModal.addEventListener('click', openModalF);
+closeModal.addEventListener('click',closeModalF)
